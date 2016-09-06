818af6ca4c9bca5da4a17f350ad3bcd390063e27
apokalyptik <apokalyptik@apokalyptik.com>
2016-09-06 02:46:26 +0000

    use dashboard middleware

--- api.go
+++ api.go
@@ -1,26 +1,54 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/NYTimes/gziphandler"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
-	"github.com/phyber/negroni-gzip/gzip"
+	nocache "github.com/rabeesh/negroni-nocache"
 	"github.com/rs/cors"
+	"github.com/uber-go/zap"
 )
 
 var router = mux.NewRouter()
 var mwh = negroni.New()
+var logger = zap.NewJSON().With(zap.String("module", "user-registry"))
+
+var cmw = cors.New(cors.Options{
+	AllowedOrigins: []string{
+		"http://127.0.0.1",
+		"http://localhost",
+	},
+	AllowedMethods: []string{
+		"GET",
+		"PUT",
+	},
+	AllowCredentials: true,
+})
+
+func mw(fn func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:*", "http://127.0.0.*"},
+		AllowCredentials: true,
+	})
+	return gziphandler.GzipHandler(
+		c.Handler(
+			negroni.New(
+				&httpLogger{},
+				negroni.NewRecovery(),
+				nocache.New(true),
+				negroni.Wrap(
+					http.HandlerFunc(fn),
+				),
+			),
+		),
+	)
+}
 
 func mindHTTP() {
-	// CORS
-	mwh.Use(cors.New(cors.Options{AllowedOrigins: []string{"fofgatming", "127.0.0.1", "localhost"}}))
-	// Recovery
-	mwh.Use(negroni.NewRecovery())
-	// Logging TODO: Replace
-	mwh.Use(negroni.NewLogger())
-	// Gzip Compression of Responses
-	mwh.Use(gzip.Gzip(gzip.DefaultCompression))
-	// Wrap the router
-	mwh.UseHandler(router)
-	// go TODO: replace
-	mwh.Run(listenOn)
+	logger.Fatal(
+		"error starting API http server",
+		zap.String("listenOn", listenOn),
+		zap.Error(http.ListenAndServe(listenOn, router)))
 }
