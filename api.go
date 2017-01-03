67332654ba157e57d3444af38247886f6f416a29
apokalyptik <apokalyptik@apokalyptik.com>
2017-01-03 19:28:07 +0000

    use custom consul package for shared functionality

--- api.go
+++ api.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"strconv"
-	"strings"
 
+	"github.com/FederationOfFathers/consul"
 	"github.com/NYTimes/gziphandler"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
-	"github.com/hashicorp/consul/api"
 	nocache "github.com/rabeesh/negroni-nocache"
 	"github.com/rs/cors"
 	"github.com/uber-go/zap"
@@ -51,17 +48,7 @@ func mw(fn func(w http.ResponseWriter, r *http.Request)) http.Handler {
 }
 
 func mindHTTP() {
-	listenParts := strings.Split(listenOn, ":")
-	listenPort, err := strconv.Atoi(listenParts[len(listenParts)-1])
-	if err != nil {
-		log.Fatalf("Unable to parse port from %s", listenOn)
-	}
-	err = consul.Agent().ServiceRegister(&api.AgentServiceRegistration{
-		Name: "user-registry",
-		Tags: []string{},
-		Port: listenPort,
-	})
-	if err != nil {
+	if err := consul.RegisterListenOn("user-registry", listenOn); err != nil {
 		panic(err)
 	}
 	logger.Fatal(
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/FederationOfFathers/consul"
 	"github.com/apokalyptik/cfg"
-	"github.com/hashicorp/consul/api"
 )
 
 var sqlURI = "user:password@tcp(127.0.0.1:3306)/fofgaming?charset=utf8mb4,utf8&collation=utf8mb4_unicode_ci"
@@ -16,19 +16,9 @@ func init() {
 	api.StringVar(&listenOn, "listen", listenOn, "Listen for api connections on")
 }
 
-var consul *api.Client
-
-func mustConsul() {
-	client, err := api.NewClient(api.DefaultConfig())
-	if err != nil {
-		panic(err)
-	}
-	consul = client
-}
-
 func main() {
 	cfg.Parse()
-	mustConsul()
+	consul.Must()
 	mindSQL()
 	go mindSeenList()
 	go mindPrivateUserList()
