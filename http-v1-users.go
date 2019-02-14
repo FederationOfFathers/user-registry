627106e10a51151e2ecf4ccfd7c0449971438afc
apokalyptik <apokalyptik@apokalyptik.com>
2019-02-14 06:02:29 +0000

    bring back /v1/users.json

--- http-v1-users.go
+++ http-v1-users.go
@@ -8,30 +8,24 @@ import (
 
 func init() {
 	router.HandleFunc("/v1/users.json", func(w http.ResponseWriter, r *http.Request) {
-		//var u = userList
-		// var s = seenList
 		var rval = map[string]struct {
 			User privateUser
 			Seen time.Time
 		}{}
-		/*
-			var maxAge = time.Now().Add(0 - (30 * 24 * time.Hour))
-			for id, t := range s {
-				if id == "USLACKBOT" {
-					continue
-				}
-				if t.Before(maxAge) {
-					continue
-				}
-				rval[id] = struct {
-					User privateUser
-					Seen time.Time
-				}{
-					User: u[id],
-					Seen: t,
-				}
+		var maxAge = time.Now().Add(0 - (30 * 24 * time.Hour))
+		for id, user := range userList {
+			t := time.Unix(user.Seen, 0)
+			if t.Before(maxAge) {
+				continue
 			}
-		*/
+			rval[id] = struct {
+				User privateUser
+				Seen time.Time
+			}{
+				User: *user,
+				Seen: t,
+			}
+		}
 		json.NewEncoder(w).Encode(rval)
 	})
 }
