30cab2c4e01bb65a44305d45451c1fa73e682fa2
apokalyptik <apokalyptik@apokalyptik.com>
2019-02-16 15:41:47 +0000

    logging cleanup. fix meta update query. fix all users select query. insert image and thumb on creation instead of at next poll

--- mysql.go
+++ mysql.go
@@ -26,8 +26,8 @@ func mustPrepare(q string) *sql.Stmt {
 }
 
 func mindSQL() {
-	wantMemberColumns := "m.`ID`,m.`xbl`,m.`destiny`,m.`seen`,m.`name`,m.`tz`,m.`discord`,IFNULL(mm1.meta_value, '') as `image`, IFNULL(mm2.meta_value, '') as `thumb`"
-	wantMemberJoin := "members m LEFT JOIN membermeta mm1 ON(m.id=mm1.id AND mm1.meta_key='image') LEFT JOIN membermeta mm2 ON( m.id=mm2.id AND mm2.meta_key='thumb')"
+	wantMemberColumns := "m.`ID`, m.`xbl`, m.`destiny`, m.`seen`, m.`name`, m.`tz`, m.`discord`, IFNULL(mm1.meta_value, '') as `image`, IFNULL(mm2.meta_value, '') as `thumb`"
+	wantMemberJoin := "members m LEFT JOIN membermeta mm1 ON(m.id=mm1.member_id AND mm1.meta_key='image') LEFT JOIN membermeta mm2 ON( m.id=mm2.member_id AND mm2.meta_key='thumb')"
 	conn, err := sql.Open("mysql", sqlURI)
 	if err != nil {
 		log.Fatal(err)
@@ -40,7 +40,7 @@ func mindSQL() {
 	getAllUsers = mustPrepare(fmt.Sprintf("SELECT %s FROM %s WHERE discord IS NOT NULL", wantMemberColumns, wantMemberJoin))
 	getDiscordUser = mustPrepare(fmt.Sprintf("SELECT %s FROM %s WHERE `discord`=?", wantMemberColumns, wantMemberJoin))
 	insertUser = mustPrepare("INSERT IGNORE INTO `members` (`discord`,`name`,`seen`,`updated_at`,`created_at`) VALUES(?,?,UNIX_TIMESTAMP(), NOW(), NOW())")
-	insertUserMeta = mustPrepare("INSERT INTO `membermeta` (`member_ID`,`meta_key`,`meta_value`) VALUES (?,?,?) ON DUPLICATE KEY UPDATE `meta_value`=`meta_value`")
+	insertUserMeta = mustPrepare("INSERT INTO `membermeta` (`member_ID`,`meta_key`,`meta_value`) VALUES (?,?,?) ON DUPLICATE KEY UPDATE `meta_value`=VALUES(`meta_value`)")
 	updateName = mustPrepare("UPDATE `members` SET `name`=?, `updated_at`=NOW() WHERE `id` = ? LIMIT 1")
 	updateSeen = mustPrepare("UPDATE `members` SET `seen`=?, `updated_at`=NOW() WHERE `id` = ? LIMIT 1")
 }
--- users.go
+++ users.go
@@ -31,49 +31,57 @@ func maybeInsertUser(discordID, nickname, image, thumb string) {
 			continue
 		}
 		if u.Name != nickname || u.DisplayName != nickname {
+			oldName := userList[id].Name
 			userList[id].DisplayName = nickname
 			userList[id].Name = nickname
-			_, err := updateName.Exec(nickname, id)
-			if err != nil {
-				log.Println("error updating nickname", err.Error(), id, nickname)
+			if _, err := updateName.Exec(nickname, id); err != nil {
+				log.Printf("Error updating name for %s to %s: %s", userList[id].Name, nickname, err.Error())
 			} else {
-				log.Println("updated nickname", id, nickname)
+				log.Printf("Updated name for %s to %s", oldName, userList[id].Name)
 			}
 		}
 		if u.Thumb != thumb {
 			userList[id].Thumb = thumb
-			_, err := insertUserMeta.Exec(id, "thumb", thumb)
-			if err != nil {
-				log.Println("error updating nickname", err.Error(), id, thumb)
+			if _, err := insertUserMeta.Exec(id, "thumb", thumb); err != nil {
+				log.Printf("Error updating thumb for %s to %s: %s", userList[id].Name, thumb, err.Error())
 			} else {
-				log.Println("updated nickname", id, thumb)
+				log.Printf("Updated thumb for %s to %s", userList[id].Name, thumb)
 			}
-
 		}
 		if u.Image != image {
 			userList[id].Image = image
-			_, err := insertUserMeta.Exec(id, "image", image)
-			if err != nil {
-				log.Println("error updating image", err.Error(), id, image)
+			if _, err := insertUserMeta.Exec(id, "image", image); err != nil {
+				log.Printf("Error updating image for %s to %s: %s", userList[id].Name, image, err.Error())
 			} else {
-				log.Println("updated image", id, image)
+				log.Printf("Updated image for %s to %s", userList[id].Name, image)
 			}
 		}
 		return
 	}
-	_, err := insertUser.Exec(discordID, nickname)
-	if err != nil {
-		log.Println("insertUser.Exec error", err.Error(), "for", discordID, nickname)
+	if _, err := insertUser.Exec(discordID, nickname); err != nil {
+		log.Printf("Error inserting user %s (%s): %s", discordID, nickname, err.Error())
 		return
 	}
 	u := new(user)
 	if err := u.fromRow(getDiscordUser.QueryRow(discordID)); err != nil {
-		log.Println("u.fromRows error", err.Error(), discordID)
+		log.Printf("Error querying user %s (%s) after insert: %s", discordID, nickname, err.Error())
 		return
 	}
 	p := u.privateUser()
 	userList[p.ID] = p
-	log.Println("new user", p)
+	userList[p.ID].Thumb = thumb
+	if _, err := insertUserMeta.Exec(p.ID, "thumb", thumb); err != nil {
+		log.Printf("Error updating thumb for inserted user %s to %s: %s", userList[p.ID].Name, thumb, err.Error())
+	} else {
+		log.Printf("Updated thumb for inserted user %s to %s", userList[p.ID].Name, thumb)
+	}
+	userList[p.ID].Image = image
+	if _, err := insertUserMeta.Exec(p.ID, "image", image); err != nil {
+		log.Printf("Error updating image for inserted user %s to %s: %s", userList[p.ID].Name, image, err.Error())
+	} else {
+		log.Printf("Updated image for inserted user %s to %s", userList[p.ID].Name, image)
+	}
+	log.Printf("Created new user id %s: %s, discord: %s, image: %s", p.ID, p.Name, p.DiscordID, p.Image)
 }
 
 func maybeUpdateSeen(discordID string) {
@@ -88,11 +96,10 @@ func maybeUpdateSeen(discordID string) {
 			return
 		}
 		userList[id].Seen = now
-		_, err := updateSeen.Exec(now, id)
-		if err != nil {
-			log.Println("error updating seen", id, now)
+		if _, err := updateSeen.Exec(now, id); err != nil {
+			log.Printf("Error updating seen for %s (%s): %s", userList[id].Name, id, err.Error())
 		} else {
-			log.Println("updated seen", userList[id].Name, now)
+			log.Printf("Updated seen for %s (%s): %d", userList[id].Name, id, now)
 		}
 		return
 	}
@@ -107,7 +114,7 @@ func loadUsersFromDatabase() {
 			for rows.Next() {
 				u := new(user)
 				if err := u.fromRows(rows); err != nil {
-					log.Println("u.fromRow error", err.Error())
+					log.Printf("Error loading user from database: %s", err.Error())
 					continue
 				}
 				p := u.privateUser()
